fac8740d4c8d1fb24bd57b5a9c8d4be2f9bd6622
wwknice <1582627404@qq.com>
2021-04-29 12:57:26 +0800

    Initial commit

---
+++ src/itheima_03/AnimlaDemo.java
@@ -0,0 +1,11 @@
+package itheima_03;
+/*
+测试类
+*/
+public class AnimlaDemo {
+    public static void main(String[] args) {
+        Animal a = new Cat();
+        a.eat();
+        a.show();
+    }
+}
